ba0716eae0693e7ac745ac2cc4ffb41932cfdc68
NiNor14 <n.normand14@gmail.com>
2018-10-17 21:00:47 +0200

    projet hello world shapproject

---
+++ testGITpullpushcomitadd/testGITpullpushcomitadd/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nicolas
+ * Date: 17/10/2018
+ * Time: 20:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testGITpullpushcomitadd
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
